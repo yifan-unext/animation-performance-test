2f5215b1be7d8df3f570a5706763e0370c4b64fe
yifan <y-liao@unext.jp>
2018-05-10 15:26:56 +0900

    Change to image thumbnails.

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-var TEST_INTERVAL = 3000;
-var TEST_DOM_COUNTS = [100, 500, 1000, 1500, 2000, 2500];
+var TEST_INTERVAL = 4000;
+var TEST_DOM_COUNTS = [5, 10, 15, 20, 25, 30, 35, 40];
 var TEST_ANIMATIONS = ['left', 'transform'];
-
+var TEST_IMAGE_SRCS = [
+    'http://imgc.nxtv.jp/img/info/tit/00034/SID0034113.png?output-quality=30&output-format=jpg',
+    'http://imgc.nxtv.jp/img/info/tit/00032/SID0032694.png?output-quality=30&output-format=jpg',
+    'http://imgc.nxtv.jp/img/info/tit/00033/SID0033606.png?output-quality=30&output-format=jpg',
+    'http://imgc.nxtv.jp/img/info/tit/00031/SID0031251.png?output-quality=30&output-format=jpg',
+    'http://imgc.nxtv.jp/img/info/tit/00034/SID0034716.png?output-quality=30&output-format=jpg',
+    'http://imgc.nxtv.jp/img/info/tit/00033/SID0033998.png?output-quality=30&output-format=jpg',
+    'http://imgc.nxtv.jp/img/info/tit/00033/SID0033992.png?output-quality=30&output-format=jpg',
+    'http://imgc.nxtv.jp/img/info/tit/00028/SID0028347.png?output-quality=30&output-format=jpg',
+    'http://imgc.nxtv.jp/img/info/tit/00028/SID0028649.png?output-quality=30&output-format=jpg',
+    'http://imgc.nxtv.jp/img/info/tit/00003/SID0003692.png?output-quality=30&output-format=jpg'
+];
 
 function createDiv(className) {
     var div = document.createElement('div');
@@ -19,6 +30,25 @@ function setDivs(count, className) {
     }
 }
 
+function createImg(className) {
+    var img = document.createElement('img');
+    img.className = className;
+    img.src = TEST_IMAGE_SRCS[Math.floor(Math.random()*10)];
+    return img;
+}
+
+function setImgs(count, className) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    for (var i=0; i<count; i++) {
+        var img = createImg(className);
+        img.style.animationDelay = (i * 0.1) + 's';
+        container.appendChild(img);
+    }
+}
+
 var container = document.getElementById('container');
 var title = document.getElementById('title');
 // title.textContent = TEST_ANIMATIONS[0];
@@ -27,8 +57,9 @@ var lastStartTime = 0;
 var domCount = 0;
 var currentAnimation = 0;
 
-title.textContent = TEST_ANIMATIONS[currentAnimation] + ' number of div: ' + TEST_DOM_COUNTS[domCount];
-setDivs(TEST_DOM_COUNTS[domCount], TEST_ANIMATIONS[currentAnimation]);
+title.textContent = TEST_ANIMATIONS[currentAnimation] + ' number of DOM elements: ' + TEST_DOM_COUNTS[domCount];
+// setDivs(TEST_DOM_COUNTS[domCount], TEST_ANIMATIONS[currentAnimation]);
+setImgs(TEST_DOM_COUNTS[domCount], TEST_ANIMATIONS[currentAnimation]);
 
 requestAnimationFrame(function animate(timestamp) {
     if (timestamp > lastStartTime + TEST_INTERVAL) {
@@ -43,7 +74,8 @@ requestAnimationFrame(function animate(timestamp) {
                 currentAnimation = 0;
             }
         }
-        setDivs(TEST_DOM_COUNTS[domCount], TEST_ANIMATIONS[currentAnimation]);
+        // setDivs(TEST_DOM_COUNTS[domCount], TEST_ANIMATIONS[currentAnimation]);
+        setImgs(TEST_DOM_COUNTS[domCount], TEST_ANIMATIONS[currentAnimation]);
         title.textContent = TEST_ANIMATIONS[currentAnimation] + ' number of div: ' + TEST_DOM_COUNTS[domCount];
     }
 
